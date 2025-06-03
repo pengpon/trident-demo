34fa9c0f5bf2fb714baee3775e32815c6d674e99
pengpon <bd102pon@gmail.com>
2025-06-03 17:46:31 +0800

    feat: add 1200 breakpoint layout and menu select js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 let navbarToggle = document.querySelector(".navbar .hamburger-menu");
-let sidebar = document.querySelector(".navbar .sidebar");
+let sidebar = document.querySelector(".header .sidebar");
+let menu = document.querySelector(".sidebar-list");
 
 navbarToggle.addEventListener("click", toggleClass);
-
 function toggleClass() {
     navbarToggle.classList.toggle('open');
     sidebar.classList.toggle('open')
+}
+
+menu.addEventListener("click", addActiveClass)
+function addActiveClass(event) {
+    let target = event.target;
+
+    // remove all active class
+    let items = document.querySelectorAll(".sidebar-list .sidebar-item");
+    items.forEach((item) => {
+        item.classList.remove("active");
+    })
+
+    // add class to target
+    target.classList.add("active")
 }
